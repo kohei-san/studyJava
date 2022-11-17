61ffd1d7c061b2b642aa2670c02e355c913f6774
kohei-san <kohei.miyatake@gmail.com>
2022-11-17 12:24:21 +0900

    mazeプログラムまで完了

---
+++ src/main/java/org/example/ForHello.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class ForHello {
+    public static void main(String[] args) {
+        for (int n = 1; n < 11; n++) {
+            System.out.println(n);
+        }
+    }
+}
---
+++ src/main/java/org/example/kakezan.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class kakezan {
+    public static void main(String[] args) {
+        for (int i = 5; i > 0; i--) {
+            System.out.print("0".repeat(i));
+            System.out.println();
+        }
+    }
+}
