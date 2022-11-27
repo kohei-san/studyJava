39ef8054fe747fd73b6a4fa56919f1cbcaa7e3a6
kohei-san <kohei.miyatake@gmail.com>
2022-11-27 10:19:59 +0900

    Method ~ Lambda Practice

--- src/main/java/org/example/StreamPracticeInt.java
+++ src/main/java/org/example/StreamPracticeInt.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import java.util.Arrays;
 import java.util.stream.IntStream;
 
 public class StreamPracticeInt {
@@ -9,5 +10,18 @@ public class StreamPracticeInt {
         var nums = new int[] {1,2,3};
 //        var list = IntStream.of(nums).mapToObj(n -> "*".repeat(n)).toList();
         IntStream.of(nums).mapToObj("*"::repeat).toList().forEach(System.out::println);
+
+        // p223問題
+        // ラムダ式　〜　メソッド参照
+        var names = new String[] {"aaa", "bbb", "ccc"};
+        var list = Arrays.stream(names).map(String::toUpperCase).toList();
+        System.out.println(list);
+
+        var list2 = list.stream().map("%sさん"::formatted).toList();
+        System.out.println(list2);
+
+        var list3 = list.stream().map(s -> s.toLowerCase()).toList();
+        System.out.println(list3);
+
     }
 }
