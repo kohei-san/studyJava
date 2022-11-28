d250ab6eb1b12a11a929a6f8b29d5a5daade6b3e
kohei-san <kohei.miyatake@gmail.com>
2022-11-29 06:43:14 +0900

    再帰処理_practice

--- src/main/java/org/example/RecursiveLoop.java
+++ src/main/java/org/example/RecursiveLoop.java
@@ -3,14 +3,14 @@ package org.example;
 public class RecursiveLoop {
     public static void main(String[] args) {
         // 再帰処理のサンプル
-        loop(0);
+        loop(3);
     }
 
     static void loop(int i) {
-        if (i >= 5) { // 再帰回数があまりに多いとStackoverflowとなる
+        if (i <= 0) { // 再帰回数があまりに多いとStackoverflowとなる
             return;
         }
         System.out.println(i);
-        loop(i + 1);
+        loop(i - 1);
     }
 }
