d422b36d8f8e8864bdc0bef628cba433e2b0f521
kohei-san <kohei.miyatake@gmail.com>
2022-12-06 07:26:53 +0900

    WebClient2

---
+++ src/main/java/org/example/WebClient2.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class WebClient2 {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+        URI uri = URI.create("https://example.com/");
+        HttpRequest req = HttpRequest.newBuilder(uri).build();
+        HttpResponse<String> response = client.send(
+                req, HttpResponse.BodyHandlers.ofString());
+        String body = response.body();
+        body.lines()
+                .limit(10)
+                .forEach(System.out::println);
+    }
+}
